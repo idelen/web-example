c92aa92350d76357a31801840368e4210b2edcdb
CJP mac <ch.jackpot@gmail.com>
2021-03-24 17:51:22 +0900

    [MacBook] ~100page

--- src/main/java/com/choi/example/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/choi/example/springboot/web/dto/HelloResponseDto.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
 @RequiredArgsConstructor
+@Getter
 public class HelloResponseDto {
     private final String name;
     private final int amount;
